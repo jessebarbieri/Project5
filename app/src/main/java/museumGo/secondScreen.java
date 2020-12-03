76a706cfe4e71a059c5fb20628adcc7ebbe7ceed
Arya Kulkarni <kulkarya@gmail.com>
2020-12-03 17:00:18 -0500

    Added ticket stuff need to add logic

--- app/src/main/java/museumGo/secondScreen.java
+++ app/src/main/java/museumGo/secondScreen.java
@@ -34,87 +34,90 @@ public class secondScreen extends AppCompatActivity {
         selectedMuseum = getdata.getStringExtra("selectedMuseum");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        setContentView(R.layout.activity_second_screen);
-
 
         Toast.makeText(secondScreen.this, selectedMuseum, Toast.LENGTH_LONG).show();
         museumPic = (ImageView) findViewById(R.id.museumPic);
 
-        if(selectedMuseum.equals("The Metropolitan Museum of Art")){
-            museumPic.setImageResource(R.drawable.metpic);
-            museum MMA = new museum(10, 20, 15);
-            museumPic.setOnClickListener(new View.OnClickListener() {
-                /**
-                 * Is called when the image is clicked on, opens the website of the selected museum
-                 * @param v, the ImageView
-                 */
-                @Override
-                public void onClick(View v) {
-                    Uri website = Uri.parse("https://www.metmuseum.org");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, website);
-                    startActivity(intent);
-                }
-            });
+        ((TextView)findViewById(R.id.textViewStudent)).setText("0");
+        textViewStudent = ((TextView)findViewById(R.id.textViewStudent));
+        textViewStudent.setText("0");
+
+        switch (selectedMuseum) {
+            case "Museum of Modern Art (MoMA)":
+                museumPic.setImageResource(R.drawable.momapic);
+                M = new museum(11, 23, 17);
+                museumPic.setOnClickListener(new View.OnClickListener() {
+                    /**
+                     * Is called when the image is clicked on, opens the website of the selected museum
+                     * @param v, the ImageView
+                     */
+                    @Override
+                    public void onClick(View v) {
+                        Uri website = Uri.parse("https://www.moma.org");
+                        Intent intent = new Intent(Intent.ACTION_VIEW, website);
+                        startActivity(intent);
+                    }
+                });
 
 
-        }
-        else if(selectedMuseum.equals("Museum of Modern Art (MoMA)")){
-            museumPic.setImageResource(R.drawable.momapic);
-            M = new museum(11, 23, 17);
-            museumPic.setOnClickListener(new View.OnClickListener() {
-                /**
-                 * Is called when the image is clicked on, opens the website of the selected museum
-                 * @param v, the ImageView
-                 */
-                @Override
-                public void onClick(View v) {
-                    Uri website = Uri.parse("https://www.moma.org");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, website);
-                    startActivity(intent);
-                }
-            });
-
-
-        }
-        else if(selectedMuseum.equals("American Museum of Natural History")){
-            museumPic.setImageResource(R.drawable.historypic);
-            M = new museum(12, 23, 16);
-            museumPic.setOnClickListener(new View.OnClickListener() {
-                /**
-                 * Is called when the image is clicked on, opens the website of the selected museum
-                 * @param v, the ImageView
-                 */
-                @Override
-                public void onClick(View v) {
+                break;
+            case "American Museum of Natural History":
+                museumPic.setImageResource(R.drawable.historypic);
+                M = new museum(12, 23, 16);
+                museumPic.setOnClickListener(new View.OnClickListener() {
+                    /**
+                     * Is called when the image is clicked on, opens the website of the selected museum
+                     * @param v, the ImageView
+                     */
+                    @Override
+                    public void onClick(View v) {
                         Uri website = Uri.parse("https://www.amnh.org");
                         Intent intent = new Intent(Intent.ACTION_VIEW, website);
                         startActivity(intent);
-                }
-            });
+                    }
+                });
 
 
-        }
-        else if(selectedMuseum.equals("Intrepid Sea, Air & Space Museum")){
-            museumPic.setImageResource(R.drawable.intrepid);
-            M = new museum(8, 17, 12);
-            museumPic.setOnClickListener(new View.OnClickListener() {
-                /**
-                 * Is called when the image is clicked on, opens the website of the selected museum
-                 * @param v, the ImageView
-                 */
-                @Override
-                public void onClick(View v) {
-                    Uri website = Uri.parse("https://m.intrepidmuseum.org");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, website);
-                    startActivity(intent);
-                }
-            });
+                break;
+            case "Intrepid Sea, Air & Space Museum":
+                museumPic.setImageResource(R.drawable.intrepid);
+                M = new museum(8, 17, 12);
+                museumPic.setOnClickListener(new View.OnClickListener() {
+                    /**
+                     * Is called when the image is clicked on, opens the website of the selected museum
+                     * @param v, the ImageView
+                     */
+                    @Override
+                    public void onClick(View v) {
+                        Uri website = Uri.parse("https://m.intrepidmuseum.org");
+                        Intent intent = new Intent(Intent.ACTION_VIEW, website);
+                        startActivity(intent);
+                    }
+                });
+
+
+                break;
+            case "The Metropolitan Museum of Art":
+                museumPic.setImageResource(R.drawable.metpic);
+                museum MMA = new museum(10, 20, 15);
+                museumPic.setOnClickListener(new View.OnClickListener() {
+                    /**
+                     * Is called when the image is clicked on, opens the website of the selected museum
+                     * @param v, the ImageView
+                     */
+                    @Override
+                    public void onClick(View v) {
+                        Uri website = Uri.parse("https://www.metmuseum.org");
+                        Intent intent = new Intent(Intent.ACTION_VIEW, website);
+                        startActivity(intent);
+                    }
+                });
+
+
+                break;
         }
 
-        textViewStudent = (TextView)findViewById(R.id.textViewStudent);
-        if(textViewStudent!=null){
-            textViewStudent.setText("0");
-        }
+
 
     }
 
@@ -126,12 +129,11 @@ public class secondScreen extends AppCompatActivity {
     {
 
         try {
-            textViewStudent.setText(String.valueOf(M.students));
             int val = M.removeTicket(Integer.parseInt(textViewStudent.getText().toString()), "Student");
             textViewStudent.setText(String.valueOf(val));
-            Toast.makeText(this, "Subtracted, Total now is " + val, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Minus 1 Student Ticket", Toast.LENGTH_LONG).show();
         }catch (NullPointerException e){
-            Toast.makeText(this, "nope nothing here", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Null", Toast.LENGTH_LONG).show();
         }
 
     }
